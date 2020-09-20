c56677db8325b0265f2acbd8a0aa4a87589a908b
Sylvie <gillissen.sylvie@gmail.com>
2020-09-20 16:04:30 +0200

    Liste des catégories
    avec le nombre de posts par catégories

--- app/modeles/categoriesModele.php
+++ app/modeles/categoriesModele.php
@@ -1,1 +1,12 @@
-<?php 
+<?php
+namespace App\Modeles\Categories;
+
+function findAll(\PDO $connexion) {
+  $sql = "SELECT cts.name, COUNT(pts.id) as nombrecat
+          FROM categories cts
+          JOIN posts pts ON cts.id=pts.categorie_id
+          GROUP BY cts.id;";
+
+  $rs = $connexion->query($sql);
+  return $rs->fetchAll(\PDO::FETCH_ASSOC); // Tableau indexé de tableaux associatifs
+}
